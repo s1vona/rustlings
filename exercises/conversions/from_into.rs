df9a8122c646a1a8960fd437f5081ffad8b04e15
s1vona <pwn4sivona@gmail.com>
2022-04-25 20:42:10 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let s: Vec<&str> = s.split(",").collect();
+            if s.len() != 2 || s[0].len() == 0 || s[1].len() == 0 {
+                Person::default()
+            } else {
+                let name = s[0].to_string();
+                match s[1].parse::<usize>() {
+                    Ok(age) => Person { name, age },
+                    Err(_) => Person::default(),
+                }
+            }
+        }
     }
 }
 
--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,15 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($($x:expr),*) => {{
+        let mut s = String::new();
+        $(
+            s = format!("Hello {}",$x);
+        )*
+        s
+    }};
+}
 
 #[cfg(test)]
 mod tests {
